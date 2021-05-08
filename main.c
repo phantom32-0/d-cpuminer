910deb9ff0b8e6b78750c5763e39f887d2916692
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2021-05-08 13:51:56 +0200

    reduce buffer sizes, fix overflows

--- main.c
+++ main.c
@@ -25,30 +25,30 @@ unsigned int hash_count = 0;
 
 char *my_itoa (int num, char *str) { // convert int to string function
 	if (str == NULL) return NULL;
-    sprintf(str, "%d", num);
-    return str;
+	sprintf(str, "%d", num);
+	return str;
 }
 
-void* hashrateCounter(void* p){
+void* hashrateCounter(void* p) {
 
-	while(1) {
+	while (1) {
 		last_hash_count = hash_count;
- 		khash_count = last_hash_count / 1000;
-  		hash_count = 0;
-  	sleep(1);
-  }
+		khash_count = last_hash_count / 1000;
+		hash_count = 0;
+		sleep(1);
+	}
 }
 
 
 int main (int argc, char **argv) {
 	const unsigned int serverport = 2811; // static server port
-	char jobmessage[1024] = "JOB,";
-	char serverreply[2048];
+	char jobmessage[64] = "JOB,revox,LOW";
+	char serverreply[128];
 	int socket_desc;
 	struct sockaddr_in server;
-	char serverversion[8];
-	char username[512] = "";
-	
+	char serverversion[3];
+	char username[64] = "";
+
 	printf("\033[1;33md-cpuminer\nMade by phantom32 and revox\n"); //color yellow
 	printf("----------\n\033[0m"); //reset color
 
@@ -75,44 +75,48 @@ int main (int argc, char **argv) {
 		return 1;
 	}
 
-	if (recv(socket_desc, serverversion, 8, 0) < 0) {
+	printf("Connected to the server\n");
+
+	if (recv(socket_desc, serverversion, 3, 0) < 0) {
 		printf("Error: Server version couldn't be received\n");
 		return 1;
 	}
 
-	printf("\nConnected to the server\nServer is on version: %s\n\n",serverversion);
+	printf("Server is on version: %s\n\n", serverversion);
 
-	strcat(jobmessage, username); //combine "JOB," and duco username
+	//strcat(jobmessage, username); //combine "JOB," and duco username
+	//strcat(jobmessage, ',LOW'); //combine "JOB," and duco username
 
-	
+
 	printf("Mining started using DUCO-S1 algorithm\n");
 
 	pthread_t id;
 	int j = 0;
 	pthread_create(&id, NULL, hashrateCounter, &j); // start hashrate counter thread
-	while(1) {
+
+	while (1) {
 		if (send(socket_desc, jobmessage, strlen(jobmessage), 0) < 0) {
 			printf("Error: Couldn't send JOB message\n");
 			return 1;
 		}
 
-		if (recv(socket_desc, serverreply, 2048, 0) < 0) {
-			printf("Error: Server version couldn't be received\n");
+		if (recv(socket_desc, serverreply, 128, 0) < 0) {
+			printf("Error: Job couldn't be received\n");
 			return 1;
 		}
 
-	    char* job = strtok (serverreply, ",");
-	    char* work = strtok (NULL, ",");
-	    char* diff = strtok (NULL, "");
+		char* job = strtok (serverreply, ",");
+		char* work = strtok (NULL, ",");
+		char* diff = strtok (NULL, "");
 
-	    //printf("\nRecived data!\n");
+		//printf("\nRecived data!\n");
 		//printf("JOB: %s\n", job);
 		//printf("JOB2: %s\n", work);
 		//printf("DIFF: %s\n", diff);
-		
-		char dLS[512] = "";
-		
-		for(int i=0; i < (100*atoi(diff))+1; i++) {
+
+		char dLS[128] = "";
+
+		for (int i = 0; i < (100 * atoi(diff)) + 1; i++) {
 			hash_count++;
 			char strintohash[128] = "";
 
@@ -124,13 +128,13 @@ int main (int argc, char **argv) {
 			//printf("String to hash: %s\n", strintohash);
 
 			unsigned char temp[SHA_DIGEST_LENGTH];
-			char buf[SHA_DIGEST_LENGTH*2];
-			memset(buf, 0x0, SHA_DIGEST_LENGTH*2);
+			char buf[SHA_DIGEST_LENGTH * 2];
+			memset(buf, 0x0, SHA_DIGEST_LENGTH * 2);
 			memset(temp, 0x0, SHA_DIGEST_LENGTH);
 			SHA1((unsigned char *)strintohash, strlen(strintohash), temp);
 			long iZ = 0;
-			for (iZ=0; iZ < SHA_DIGEST_LENGTH; iZ++) {
-		    	sprintf((char*)&(buf[iZ*2]), "%02x", temp[iZ]);
+			for (iZ = 0; iZ < SHA_DIGEST_LENGTH; iZ++) {
+				sprintf((char*) & (buf[iZ * 2]), "%02x", temp[iZ]);
 			}
 
 			//printf("Hashed res    : %s\n", buf);
@@ -139,23 +143,23 @@ int main (int argc, char **argv) {
 			if (strcmp(work, buf) == 0) {
 				//printf("Found share! %s\n", dLS);
 
-				if(send(socket_desc, dLS, strlen(dLS), 0) < 0) { //send result
+				if (send(socket_desc, dLS, strlen(dLS), 0) < 0) { //send result
 					printf("Error: Couldn't send result\n");
 					return 1;
 				}
 
-				char feedback[512] = "";
-				if(recv(socket_desc, feedback, 2048, 0) < 0) { // receive feedback
+				char feedback[8] = "";
+				if (recv(socket_desc, feedback, 8, 0) < 0) { // receive feedback
 					printf("Error: Feedback couldn't be received\n");
 					return 1;
 				}
 
 				//printf("Feedback: %s\n", feedback);
-				if (strcmp("GOOD", feedback)==0 || strcmp("BLOCK", feedback)==0) {
+				if (strcmp("GOOD\n", feedback) == 0 || strcmp("BLOCK\n", feedback) == 0) {
 					acceptedShares++;
 					printf("Accepted share #%i (%s) %ikH/s\n", acceptedShares, dLS, khash_count);
 
-				} else if (strcmp("INVU", feedback)==0) {
+				} else if (strcmp("INVU\n", feedback) == 0) {
 					printf("Error: Incorrect username\n");
 					return 1;
 
